2a4b88abeccb183ed5328cb8abc598c12847b094
Mahamudul Islam Shawcha <shawcha2007040@stud.kuet.ac.bd>
2024-05-23 23:15:11 +0600

    all layout is made

--- app/src/main/java/com/nafsoft/aspireacademy/MainActivity.java
+++ app/src/main/java/com/nafsoft/aspireacademy/MainActivity.java
@@ -2,13 +2,26 @@ package com.nafsoft.aspireacademy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.nafsoft.aspireacademy.examsection.views.ListFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button examcorner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        examcorner=findViewById(R.id.examCorner);
+        examcorner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this, ListFragment.class);
+                startActivity(intent);
+            }
+        });
     }
 }
