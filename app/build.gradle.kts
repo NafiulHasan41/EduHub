2e9f54402ed643d133f7091cf08355c0e605aa7c
Swagoto das <swagotodas3@gmail.com>
2024-05-24 03:02:47 +0600

    starting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.nafsoft.aspireacademy"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.nafsoft.aspireacademy"
