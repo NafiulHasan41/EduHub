d59c158f5cd4e533c391e164f49c6c797e6b0290
Swagoto das <swagotodas3@gmail.com>
2024-05-24 14:01:49 +0600

    ready UI

---
+++ app/src/main/java/com/nafsoft/aspireacademy/pdfsection/pdfSection.java
@@ -0,0 +1,16 @@
+package com.nafsoft.aspireacademy.pdfsection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nafsoft.aspireacademy.R;
+
+public class pdfSection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_section);
+    }
+}
