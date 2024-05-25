e06baa915e3bad3d4a2cc66eb91edfcff80cebeb
Nafiul Hasan <nhashik40@gmail.com>
2024-05-25 17:01:44 +0600

    completed using factory method to video section

--- app/src/main/java/com/nafsoft/aspireacademy/videosection/ShowVideo.java
+++ app/src/main/java/com/nafsoft/aspireacademy/videosection/ShowVideo.java
@@ -1,43 +1,30 @@
 package com.nafsoft.aspireacademy.videosection;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.MediaController;
 import android.widget.VideoView;
-
 import com.nafsoft.aspireacademy.R;
 
 public class ShowVideo extends AppCompatActivity {
 
-    VideoView showVideo;
-  MediaController mediaController;
-
-  String url1 = "https://firebasestorage.googleapis.com/v0/b/aspireacademy-software-project.appspot.com/o/Videos%2FintegrationClass1.mp4?alt=media&token=adab9072-9f59-4456-9b2f-8d5fed25f7fb";
+    private VideoView showVideo;
+    private String url1 = "https://firebasestorage.googleapis.com/v0/b/aspireacademy-software-project.appspot.com/o/Videos%2FintegrationClass1.mp4?alt=media&token=adab9072-9f59-4456-9b2f-8d5fed25f7fb";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_video);
-
         showVideo = findViewById(R.id.videoView);
 
 
         Uri uri = Uri.parse(url1);
 
-        showVideo.setVideoURI(uri);
 
-        mediaController = new MediaController(this);
+        VideoComponentFactory.setupVideoView(showVideo, uri);
+        VideoComponentFactory.setupMediaController(this, showVideo);
 
-        mediaController.setMediaPlayer(showVideo);
-
-        showVideo.setMediaController(mediaController);
         showVideo.requestFocus();
-
         showVideo.start();
-
-
-
     }
 }
